ca1d21580537f3a8c7dc2447c5903a619771c9b2
leachuk <stew.leach@gmail.com>
2013-09-12 18:52:30 +1000

    Adding basic application source
    Adds data to a remote couchDb repo using pouchDb

---
+++ www/js/index.js
@@ -0,0 +1,130 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var remoteCouch = 'http://192.168.216.157:5984/phonegap-test1';
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+        
+        //var db = new PouchDB('phonegap-test1');
+        
+        Pouch('phonegap-test1', {}, function (err, db) {
+              if (err) {
+              console.log("Error while creating database: " + err)
+              for( var e in err) {
+              console.log("Error element: " + e + " -> "  + err[e]);
+              }
+              return;
+              }
+              console.log("Successfully made new database");
+              database = db;
+              db.post({_id: 'doc2', title: 'Cony Island Baby' }, {}, function (err, response) {
+                      if (!err){
+                      console.log("created doc: " + response.ok);
+                      console.log("doc id:  " + response.id)
+                      console.log("rev: " + response.rev)
+                      }else{
+                      console.log(err);
+                      }
+              });
+              
+              if (remoteCouch){
+              app.syncToRemotePouchDb(db,remoteCouch);
+              console.log('Synced data to couch db');
+              }
+              
+              var addButton = document.getElementById('addButton');
+              addButton.addEventListener('click',function(){
+                                            app.putData(db);
+                                         }, false);
+              
+              var syncButton = document.getElementById('syncButton');
+              syncButton.addEventListener('click',function(){
+                                         app.syncToRemotePouchDb(db,remoteCouch);
+                                         }, false);
+              
+        });
+        var todo = {
+            _id: new Date().toISOString(),
+            title: "test data title",
+            completed: false
+        };
+        /*
+        db.put(todo, function callback(err, result) {
+            if (!err) {
+               console.log('Successfully posted a todo!');
+            }
+        });
+        */ 
+
+        
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+    
+    syncToRemotePouchDb: function(db,remoteCouch) {
+        var opts = {continuous: false, complete: app.syncError};
+        db.replicate.to(remoteCouch, opts);
+        db.replicate.from(remoteCouch, opts);
+    },
+    syncError: function(){
+        console.log('PouchDB error syncing file');
+    },
+    putData: function(db){
+        db.post(
+                {
+                _id: new Date().toISOString(),
+                title: 'Cony Island Baby'
+                }, {}, function (err, response) {
+        if (!err){
+        console.log("created doc: " + response.ok);
+        console.log("doc id:  " + response.id)
+        console.log("rev: " + response.rev)
+        }else{
+        console.log(err);
+        }
+        });
+        
+        //console.log("putData1");
+    }
+    
+};
